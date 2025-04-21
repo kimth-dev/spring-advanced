9acadae4e185f855c4ab56880541af53246ba347
김태형 <dkdlgusghrm1@gmail.com>
2025-04-21 11:18:50 +0900

    refactor(user): UserService에서 비밀번호 유효성 검사를 제거하고 DTO에서 처리하도록 변경

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -4,14 +4,21 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.example.expert.domain.common.exception.InvalidRequestException;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserChangePasswordRequest {
 
-    @NotBlank
     private String oldPassword;
-    @NotBlank
     private String newPassword;
+
+    public void validate() {
+        if (newPassword == null || newPassword.length() < 8 ||
+                !newPassword.matches(".*\\d.*") ||
+                !newPassword.matches(".*[A-Z].*")) {
+            throw new InvalidRequestException("새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다.");
+        }
+    }
 }
